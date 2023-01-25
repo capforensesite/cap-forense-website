8e48fee76e12221816c68e57612627872372e696
Victor Rodrigues <victor.men.dev@gmail.com>
2023-01-25 20:55:11 -0300

    Capa pronta

---
+++ src/components/Capa/index.styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { mobileSize } from '../../styles/global.constants'
+
+export const Capa = styled.section`
+  width: 90%;
+  max-width: 1120px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 5rem;
+  width: 100vw;
+  margin: 10rem 0;
+
+  h1 {
+    color: white;
+    font-size: 8rem;
+  }
+
+  img {
+    width: 45rem;
+  }
+
+  ${mobileSize} {
+    width: 100%;
+
+    h1 {
+      font-size: 3rem;
+    }
+
+    img {
+      width: 30rem;
+    }
+  }
+`
---
+++ src/styles/global.constants.ts
@@ -0,0 +1,13 @@
+// Colors
+// export const redPink = '#ff5757'
+// export const purpleBlue = '#343578'
+// export const green = '#1a967c'
+// export const gray = '#e3eef0'
+
+// Sizes
+export const maxWidthScreenDesktop = '1120px'
+export const maxWidthScreenTablet = '780px'
+export const maxWidthScreenMobile = '300px'
+export const desktopSize = '@media (min-width: 1120px)'
+export const tabletSize = '@media (min-width: 738px) and (max-width: 1119px)'
+export const mobileSize = '@media (min-width: 250px) and (max-width: 738px)'
